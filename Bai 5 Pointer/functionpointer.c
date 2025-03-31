f907292843e50d11e64a55e9464496c23dd57615
Hoang Long Vu <167751897+Hoangvu3108@users.noreply.github.com>
2025-03-31 11:20:51 +0700

    Delete Bai 5 Pointer directory

--- Bai 5 Pointer/functionpointer.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void print(){
-	printf("Hello Word");
-}
-int main(){
-	void (*ptr)();	
-	ptr= print;// gán địa chỉ hàm print cho con trỏ ptr
-	ptr(); // gọi con trỏ ptr để thực hiện hàm print
-}
